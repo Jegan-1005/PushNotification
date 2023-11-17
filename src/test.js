82ed06bf1df2726dc8b9ccd5a0626e975dd9ebbf
jegan <jegan@wealwin.com>
2023-11-17 11:59:50 +0530

    in-app messaging

--- src/test.js
+++ src/test.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from "react";
 import {
   SafeAreaView,
   ScrollView,
@@ -9,12 +9,13 @@ import {
   View,
   Alert,
   PermissionsAndroid,
-} from 'react-native';
-import messaging from '@react-native-firebase/messaging';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+} from "react-native";
+import messaging from "@react-native-firebase/messaging";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+import PushNotification from "react-native-push-notification";
 
 const Test = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const isDarkMode = useColorScheme() === "dark";
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
@@ -27,9 +28,11 @@ const Test = () => {
   const requestUserPermission = async () => {
     const granted = await PermissionsAndroid.request(
       PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+      PermissionsAndroid.PERMISSIONS.SCHEDULE_EXACT_ALARM,
+      PermissionsAndroid.PERMISSIONS.USE_EXACT_ALARM,
       {
-        title: 'Allow app to get notification',
-      },
+        title: "Allow app to get notification",
+      }
     );
     if (granted === PermissionsAndroid.RESULTS.GRANTED) {
       const authStatus = await messaging().requestPermission();
@@ -39,46 +42,60 @@ const Test = () => {
       if (enabled) {
         getInitialNotification(enabled);
       } else {
-        console.log('Failed token status:', authStatus);
+        console.log("Failed token status:", authStatus);
       }
     } else {
-      console.log('permission denied');
+      console.log("permission denied");
     }
   };
 
-  const getInitialNotification = enabled => {
-    if (enabled) {
-      messaging()
-        .getToken()
-        .then(token => {
-          console.log(token);
-          const unsubscribe = messaging().onMessage(async remoteMessage => {
-            Alert.alert(
-              'A new FCM message arrived!',
-              JSON.stringify(remoteMessage),
-            );
+  const getInitialNotification = (enabled) => {
+    PushNotification.createChannel(
+      {
+        channelId: "channel-id", // (required)
+        channelName: "My channel", // (required)
+        channelDescription: "A channel to categorise your notifications",
+        playSound: false,
+        soundName: "default",
+        importance: 4,
+        vibrate: true,
+      },
+      (created) => {
+        messaging()
+          .getToken()
+          .then((token) => {
+            console.log(token, "tokennnn");
+            const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+              console.log("A new FCM message", JSON.stringify(remoteMessage));
+              PushNotification.localNotification({
+                channelId: "channel-id",
+                message: remoteMessage.notification.body,
+                title: remoteMessage.notification.title,
+              });
+            });
+            return unsubscribe;
           });
-
-          return unsubscribe;
-        });
-    }
+      }
+    );
   };
 
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        barStyle={isDarkMode ? "light-content" : "dark-content"}
         backgroundColor={backgroundStyle.backgroundColor}
       />
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
+        style={backgroundStyle}
+      >
         <View
           style={{
             flex: 1,
-            alignSelf: 'center',
-            justifyContent: 'center',
-          }}>
+            alignSelf: "center",
+            justifyContent: "center",
+          }}
+        >
           <Text>Push notification test</Text>
         </View>
       </ScrollView>
@@ -93,15 +110,15 @@ const styles = StyleSheet.create({
   },
   sectionTitle: {
     fontSize: 24,
-    fontWeight: '600',
+    fontWeight: "600",
   },
   sectionDescription: {
     marginTop: 8,
     fontSize: 18,
-    fontWeight: '400',
+    fontWeight: "400",
   },
   highlight: {
-    fontWeight: '700',
+    fontWeight: "700",
   },
 });
 
